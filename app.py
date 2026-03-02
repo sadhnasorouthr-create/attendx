d405b1b102d968b81c45b0a188644b236b0f72b2
Sadhna Sorout <sadnasorout-create sadnasorouthr@gmail.com>
2026-03-02 20:54:31 +0530

    final flask fix

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AttendX is Live 🚀"
+    return "App is running ✅"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
